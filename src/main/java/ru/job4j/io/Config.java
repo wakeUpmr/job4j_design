7948449ed286de42b5f0094568e42c1bea39f959
wakeUpmr <egohio.work@gmail.com>
2023-10-28 15:57:26 +0300

    6. Читаем файл конфигурации [#858]

---
+++ src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,58 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
+
+public class Config {
+
+    private final String path;
+    private final Map<String, String> values = new HashMap<>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        String str;
+        try (BufferedReader reader = new BufferedReader(new FileReader(this.path))) {
+            while ((str = reader.readLine()) != null) {
+                if (!str.isBlank() && !str.startsWith("#")) {
+                    if (!str.contains("=")) {
+                        throw new IllegalArgumentException("Pair(%s) does not contain an equals sign".formatted(str));
+                    }
+                    String[] line = str.split("=", 2);
+                    if (line[0].isBlank() || line[1].isBlank()) {
+                        throw new IllegalArgumentException("Empty key or value pair(%s)".formatted(str));
+                    }
+                    values.put(line[0], line[1]);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+        return values.get(key);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().forEach(out::add);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Config("data/app.properties"));
+    }
+
+}
---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,71 @@
+package ru.job4j.io;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+class ConfigTest {
+
+    @Test
+    void whenPairWithoutComment() {
+        String path = "./data/pair_without_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name")).isEqualTo("Egor Krivonos");
+    }
+
+    @Test
+    void whenPairWhitComment() {
+        String path = ".\\data\\pair_with_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name")).isEqualTo("Egor Krivonos");
+    }
+
+    @Test
+    void whenPairWithoutAnEqualsSign() {
+        String path = ".\\data\\pair_without_an_equals_sign.properties";
+        Config config = new Config(path);
+        assertThatThrownBy(config::load)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageMatching("^.+")
+                .hasMessageContaining("Pair(nameEgor Krivonos) does not contain an equals sign");
+    }
+
+    @Test
+    void whenPairDoesntContainAKey() {
+        String path = ".\\data\\pair_without_key.properties";
+        Config config = new Config(path);
+        assertThatThrownBy(config::load)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageMatching("^.+")
+                .hasMessageContaining("Empty key or value pair(=Egor Krivonos)");
+    }
+
+    @Test
+    void whenPairDoesntContainsAValue() {
+        String path = ".\\data\\pair_without_value.properties";
+        Config config = new Config(path);
+        assertThatThrownBy(config::load)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageMatching("^.+")
+                .hasMessageContaining("Empty key or value pair(name=)");
+    }
+
+    @Test
+    void whenOnlyEqualSign() {
+        String path = ".\\data\\equal_sign.properties";
+        Config config = new Config(path);
+        assertThatThrownBy(config::load)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageMatching("^.+")
+                .hasMessageContaining("Empty key or value pair(=)");
+    }
+
+    @Test
+    void whenPairConteinsTwoEqualSigns() {
+        String path = ".\\data\\two_equal_signs.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name")).isEqualTo("Egor Krivonos=Seems like a nice man");
+    }
+}
