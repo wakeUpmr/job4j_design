240eb21d3321e32c4f18ba4a0341b810fe63be85
wakeUpmr <egohio.work@gmail.com>
2023-10-29 15:43:42 +0300

    7. Анализ доступности сервера [#859]

---
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -0,0 +1,31 @@
+package ru.job4j.io;
+
+import java.io.*;
+
+public class Analysis {
+    public void unavailable(String source, String target) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(source));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(target))) {
+            String str;
+            boolean flag = false;
+            while ((str = reader.readLine()) != null) {
+                String[] line = str.split(" ");
+                boolean isWork = Integer.parseInt(line[0]) < 400;
+                if (!isWork && !flag) {
+                    writer.append(line[1]).append(";");
+                    flag = true;
+                }
+                if (isWork && flag) {
+                    writer.append(line[1]).append(";").append(System.lineSeparator());
+                    flag = false;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+        Analysis analysis = new Analysis();
+        analysis.unavailable("data/server.log", "data/target.csv");
+    }
+}
