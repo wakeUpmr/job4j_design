f7d0fa5158bdfa809ce9551463321fdecadc11e7
wakeUpmr <egohio.work@gmail.com>
2023-11-04 16:38:30 +0300

    6. Перевернуть связанный список [#161]

---
+++ src/test/java/ru/job4j/collection/RevertLinkedTest.java
@@ -0,0 +1,48 @@
+package ru.job4j.collection;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+class RevertLinkedTest {
+    private RevertLinked<Integer> linked;
+
+    @BeforeEach
+    void init() {
+        linked = new RevertLinked<>();
+    }
+
+    @Test
+    void whenSize0ThenReturnFalse() {
+        assertThat(linked.revert()).isFalse();
+    }
+
+    @Test
+    void whenSize1ThenReturnFalse() {
+        linked.add(1);
+        assertThat(linked.revert()).isFalse();
+    }
+
+    @Test
+    void whenAddAndRevertTrue() {
+        linked.add(1);
+        linked.add(2);
+        linked.add(3);
+        linked.add(4);
+        assertThat(linked).containsSequence(1, 2, 3, 4);
+        assertThat(linked.revert()).isTrue();
+        assertThat(linked).containsSequence(4, 3, 2, 1);
+    }
+
+    @Test
+    void whenNextInEmptyListThenExceptionThrown() {
+        Iterator<Integer> it = linked.iterator();
+        assertThatThrownBy(it::next)
+                .isInstanceOf(NoSuchElementException.class);
+    }
+}
