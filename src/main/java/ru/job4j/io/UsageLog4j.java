22a4cf6d169d1ded0c1cde80665fbdbad76efb5a
wakeUpmr <egohio.work@gmail.com>
2024-01-04 17:57:45 +0300

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,10 +8,24 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        short shortPrim = 5;
+        int intPrim = 33000;
+        long longPrim = 3000000000L;
+        double doublePrim = 10.25;
+        float floatPrim = 20.25F;
+        char charPrim = 'a';
+        boolean boolPrim = true;
+        byte bytePrim = 127;
+
+        LOG.debug("short primitive : {}", shortPrim);
+        LOG.debug("integer primitive : {}", intPrim);
+        LOG.debug("long primitive : {}", longPrim);
+        LOG.debug("double primitive : {}", doublePrim);
+        LOG.debug("float primitive : {}", floatPrim);
+        LOG.debug("char primitive : {}", charPrim);
+        LOG.debug("boolean primitive : {}", boolPrim);
+        LOG.debug("byte primitive : {}", bytePrim);
+
+
     }
 }
